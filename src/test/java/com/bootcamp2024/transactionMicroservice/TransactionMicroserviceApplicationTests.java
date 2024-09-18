663de1bbc8634acf8944f3360287ef4cb5d01cb0
angel andres miranda castillo <mirandaangel83@gmail.com>
2024-09-18 17:58:57 -0500

    initial Spring Boot project

---
+++ src/test/java/com/bootcamp2024/transactionMicroservice/TransactionMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp2024.transactionMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
